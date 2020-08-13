c5edd6c4f7a0f440d9be6afcf5bf678df4cb115a
Armin Herling <armin.herling@gmail.com>
2020-08-13 22:11:27 +0200

    Finished tests and implementation

--- Prime-Factors.Tests/PrimeFactorsTests.cs
+++ Prime-Factors.Tests/PrimeFactorsTests.cs
@@ -1,13 +1,104 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace Prime_Factors.Tests
 {
     public class PrimeFactorsTests
     {
         [Test]
-        public void Test1()
+        public void PrimeFactorsOf0_Returns_EmptyList()
         {
-            Assert.Pass();
+            Assert.AreEqual(
+                new List<int>(),
+                PrimeFactors.Of(0));
+        }
+
+        [Test]
+        public void PrimeFactorsOf1_Returns_EmptyList()
+        {
+            Assert.AreEqual(
+                new List<int>(),
+                PrimeFactors.Of(1));
+        }
+
+        [Test]
+        public void PrimeFactorsOf2_Returns_2()
+        {
+            Assert.AreEqual(
+                new List<int> { 2 },
+                PrimeFactors.Of(2));
+        }
+
+        [Test]
+        public void PrimeFactorsOf3_Returns_3()
+        {
+            Assert.AreEqual(
+                new List<int> { 3 },
+                PrimeFactors.Of(3));
+        }
+
+        [Test]
+        public void PrimeFactorsOf4_Returns_2_2()
+        {
+            Assert.AreEqual(
+                new List<int> { 2, 2 },
+                PrimeFactors.Of(4));
+        }
+
+        [Test]
+        public void PrimeFactorsOf5_Returns_5()
+        {
+            Assert.AreEqual(
+                new List<int> { 5 },
+                PrimeFactors.Of(5));
+        }
+
+        [Test]
+        public void PrimeFactorsOf6_Returns_2_3()
+        {
+            Assert.AreEqual(
+                new List<int> { 2, 3 },
+                PrimeFactors.Of(6));
+        }
+
+        [Test]
+        public void PrimeFactorsOf7_Returns_7()
+        {
+            Assert.AreEqual(
+                new List<int> { 7 },
+                PrimeFactors.Of(7));
+        }
+
+        [Test]
+        public void PrimeFactorsOf8_Returns_2_2_2()
+        {
+            Assert.AreEqual(
+                new List<int> { 2, 2, 2 },
+                PrimeFactors.Of(8));
+        }
+
+        [Test]
+        public void PrimeFactorsOf9_Returns_3_3()
+        {
+            Assert.AreEqual(
+                new List<int> { 3, 3 },
+                PrimeFactors.Of(9));
+        }
+
+        [Test]
+        public void PrimeFactorsOf10_Returns_2_5()
+        {
+            Assert.AreEqual(
+                new List<int> { 2, 5 },
+                PrimeFactors.Of(10));
+        }
+
+        [Test]
+        public void PrimeFactorsOfHugeProduct_ReturnsCorrectResult()
+        {
+            Assert.AreEqual(
+                new List<int> { 2, 3, 5, 5, 7, 11, 13, 13 },
+                PrimeFactors.Of(2 * 3 * 5 * 5 * 7 * 11 * 13 * 13));
         }
     }
 }
--- Prime-Factors/PrimeFactors.cs
+++ Prime-Factors/PrimeFactors.cs
@@ -1,8 +1,17 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace Prime_Factors
 {
-    public class Class1
+    public static class PrimeFactors
     {
+        public static List<int> Of(int value)
+        {
+            var primes = new List<int>();
+            for (int factor = 2; value > 1; factor++)
+                for (; value % factor == 0; value /= factor)
+                    primes.Add(factor);
+
+            return primes;
+        }
     }
 }
